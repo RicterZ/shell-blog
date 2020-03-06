15fe76c1d14fc7b1ff7202df46cd7309b2267530
RicterZ <ricterzheng@gmail.com>
2020-03-06 10:11:05 +0800

    update

--- header.sh
+++ header.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Content-type: text/html"
 echo "X-Powered-By: shell-blog"
+echo "X-Hacked-By: koko"
 echo ""
 echo -n "<html>"
 echo -n "<head><title>Ricter's Blog</title><style>a:visited{color:blue;}</style></head>"
