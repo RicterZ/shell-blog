523a42216e54bc49780101097ec0414d8041427b
Ricter Z <ricterzheng@gmail.com>
2022-05-02 16:18:39 +0800

    update footer and VULNERABILITY_REPORTING_POLICY

--- footer.sh
+++ footer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "-------------------------------------------------------------------------"
+echo "* All the contents within the blog is licensed under the CC BY-NC-ND 4.0"
+echo "-------------------------------------------------------------------------"
 echo -n "</code></pre>"
 echo -n "</body>"
 echo -n "</html>"
